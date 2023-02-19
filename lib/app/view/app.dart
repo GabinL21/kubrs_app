4dd75736b303bfb3fc6626280518f0c4c7ec266b
gabin_laigle <gabin.laigle21@gmail.com>
2023-02-20 00:46:51 +0100

    feat: use tabular figures

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kubrs_app/l10n/l10n.dart';
@@ -31,6 +33,7 @@ class App extends StatelessWidget {
               color: Color(0xFFFFFFFF),
               fontSize: 64,
               fontWeight: FontWeight.w700,
+              fontFeatures: [FontFeature.tabularFigures()],
             ),
           ),
         ),
