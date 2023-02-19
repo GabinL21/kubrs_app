2e807e55eed579e26ad9f58fd946b080d2b330bf
gabin_laigle <gabin.laigle21@gmail.com>
2023-02-20 00:27:27 +0100

    feat: use google fonts

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:kubrs_app/l10n/l10n.dart';
 import 'package:kubrs_app/timer/timer.dart';
 
@@ -24,6 +25,7 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch(
           backgroundColor: const Color(0xFF121212),
         ),
+        textTheme: GoogleFonts.montserratTextTheme(),
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
