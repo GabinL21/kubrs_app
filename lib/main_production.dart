1e53267845a55c27c3b72b341c75d1a9ad7e7966
gabin_laigle <gabin.laigle21@gmail.com>
2023-03-11 18:34:43 +0100

    feat: add firebase initialization

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:kubrs_app/app/app.dart';
 import 'package:kubrs_app/bootstrap.dart';
+import 'package:kubrs_app/firebase_options.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await bootstrap(() => const App());
 }
