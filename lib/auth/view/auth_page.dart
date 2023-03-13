41bd8d3c5f75bca96a644b8e1dcc3e129ab66aec
gabin_laigle <gabin.laigle21@gmail.com>
2023-03-13 01:54:53 +0100

    refactor: remove unused import

--- lib/auth/view/auth_page.dart
+++ lib/auth/view/auth_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kubrs_app/auth/bloc/auth_bloc.dart';
-import 'package:kubrs_app/auth/repository/auth_repository.dart';
 import 'package:kubrs_app/timer/timer.dart';
 
 class AuthPage extends StatelessWidget {
