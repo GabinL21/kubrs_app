cdaab49fb060ab9c320910ab3f04421bea116032
gabin_laigle <gabin.laigle21@gmail.com>
2023-04-10 15:25:23 +0200

    refactor: extract theme

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:kubrs_app/app/view/app_theme.dart';
 import 'package:kubrs_app/auth/bloc/auth_bloc.dart';
 import 'package:kubrs_app/auth/repository/auth_repository.dart';
 import 'package:kubrs_app/auth/view/auth_page.dart';
@@ -18,7 +19,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: _getThemeData(),
+      theme: AppTheme.themeData,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
@@ -50,52 +51,6 @@ class App extends StatelessWidget {
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  ThemeData _getThemeData() {
-    const colorScheme = ColorScheme.dark(
-      background: Color.fromRGBO(13, 13, 13, 1),
-      primary: Color.fromRGBO(18, 18, 18, 1),
-      onPrimary: Color.fromRGBO(255, 255, 255, 1),
-      tertiary: Color.fromRGBO(247, 241, 149, 1),
-      error: Color.fromRGBO(247, 149, 149, 1),
-    );
-    return ThemeData(
-      colorScheme: colorScheme,
-      scaffoldBackgroundColor: colorScheme.background,
-      textTheme: GoogleFonts.montserratTextTheme(
-        TextTheme(
-          displayLarge: TextStyle(
-            color: colorScheme.onBackground,
-            fontSize: 64,
-            fontWeight: FontWeight.w700,
-            fontFeatures: const [FontFeature.tabularFigures()],
-          ),
-          displayMedium: TextStyle(
-            color: colorScheme.onBackground,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-            fontFeatures: const [FontFeature.tabularFigures()],
-          ),
-        ),
-      ),
-      appBarTheme: AppBarTheme(
-        backgroundColor: colorScheme.primary,
-        elevation: 0,
-      ),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor: colorScheme.primary,
-        selectedItemColor: colorScheme.tertiary,
-        unselectedItemColor: colorScheme.onPrimary,
-        elevation: 0,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-      ),
-      drawerTheme: DrawerThemeData(
-        backgroundColor: colorScheme.primary,
-        elevation: 0,
       ),
     );
   }
