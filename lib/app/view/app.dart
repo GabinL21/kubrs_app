e1c9cf0c45c5d4e75a10ffa04e27dbe305007920
gabin_laigle <gabin.laigle21@gmail.com>
2023-04-10 15:36:23 +0200

    refactor: use multi repository and bloc providers

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,9 +1,6 @@
-import 'dart:ui';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:kubrs_app/app/view/app_theme.dart';
 import 'package:kubrs_app/auth/bloc/auth_bloc.dart';
 import 'package:kubrs_app/auth/repository/auth_repository.dart';
@@ -23,32 +20,40 @@ class App extends StatelessWidget {
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
-      home: RepositoryProvider(
-        create: (_) => AuthRepository(),
-        child: BlocProvider(
-          create: (context) => AuthBloc(
-            authRepository: RepositoryProvider.of<AuthRepository>(context),
+      home: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<AuthRepository>(
+            create: (_) => AuthRepository(),
           ),
-          child: RepositoryProvider(
+          RepositoryProvider<UserRepository>(
             create: (_) => UserRepository(),
-            child: BlocProvider(
+          ),
+        ],
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider<AuthBloc>(
+              create: (context) => AuthBloc(
+                authRepository: RepositoryProvider.of<AuthRepository>(context),
+              ),
+            ),
+            BlocProvider<UserBloc>(
               create: (context) => UserBloc(
                 userRepository: RepositoryProvider.of<UserRepository>(context),
               ),
-              child: StreamBuilder<User?>(
-                stream: FirebaseAuth.instance.authStateChanges(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (snapshot.hasData) {
-                    return const TimerPage();
-                  }
-                  return const AuthPage();
-                },
-              ),
             ),
+          ],
+          child: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasData) {
+                return const TimerPage();
+              }
+              return const AuthPage();
+            },
           ),
         ),
       ),
