b28bb53ee735757531fd2bffea37d910c3b06243
gabin_laigle <gabin.laigle21@gmail.com>
2023-04-10 17:55:55 +0200

    test: add duration formatter tests

--- lib/timer/view/timer_text.dart
+++ lib/timer/view/timer_text.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kubrs_app/timer/bloc/timer_bloc.dart';
+import 'package:kubrs_app/timer/utils/duration_formatter.dart';
 
 class TimerText extends StatelessWidget {
   const TimerText({super.key});
@@ -8,25 +9,12 @@ class TimerText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final duration = context.select((TimerBloc bloc) => bloc.state.duration);
-    final textString = _getTimerTextString(duration);
+    final textString = DurationFormatter.format(duration);
     final textStyle = _getTimerTextStyle(context);
     return Text(
       textString,
       style: textStyle,
     );
-  }
-
-  String _getTimerTextString(Duration duration) {
-    final minutesStr = duration.inMinutes.toString();
-    final secondsStr =
-        duration.inSeconds.remainder(60).toString().padLeft(2, '0');
-    final millisecondsStr = (duration.inMilliseconds.remainder(1000) / 10)
-        .floor()
-        .toString()
-        .padLeft(2, '0');
-    var textStr = '$secondsStr.$millisecondsStr';
-    if (duration > const Duration(minutes: 1)) textStr = '$minutesStr:$textStr';
-    return textStr;
   }
 
   TextStyle _getTimerTextStyle(BuildContext context) {
---
+++ test/timer/utils/duration_formatter_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kubrs_app/timer/utils/duration_formatter.dart';
+
+void main() {
+  group('DurationFormatter', () {
+    test('formats duration less than 1 minute correctly', () {
+      const duration = Duration(seconds: 30, milliseconds: 500);
+      expect(DurationFormatter.format(duration), equals('30.50'));
+    });
+
+    test('formats duration greater than 1 minute correctly', () {
+      const duration = Duration(minutes: 1, seconds: 30, milliseconds: 500);
+      expect(DurationFormatter.format(duration), equals('1:30.50'));
+    });
+
+    test('formats duration greater than 10 minutes correctly', () {
+      const duration = Duration(minutes: 10, seconds: 30, milliseconds: 500);
+      expect(DurationFormatter.format(duration), equals('10:30.50'));
+    });
+
+    test('formats duration less than 1 second correctly', () {
+      const duration = Duration(milliseconds: 500);
+      expect(DurationFormatter.format(duration), equals('00.50'));
+    });
+
+    test('formats duration of 1 second correctly', () {
+      const duration = Duration(seconds: 1);
+      expect(DurationFormatter.format(duration), equals('01.00'));
+    });
+
+    test('formats duration of 1 minute correctly', () {
+      const duration = Duration(minutes: 1);
+      expect(DurationFormatter.format(duration), equals('1:00.00'));
+    });
+
+    test('formats duration of 1 hour correctly', () {
+      const duration = Duration(hours: 1);
+      expect(DurationFormatter.format(duration), equals('60:00.00'));
+    });
+  });
+}
