4f53ce722a905fe861ea10ad311215e6eaaa7ae5
gabin_laigle <gabin.laigle21@gmail.com>
2023-07-10 02:15:46 +0200

    feat: lock screen orientation

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:kubrs_app/app/app.dart';
 import 'package:kubrs_app/bootstrap.dart';
 import 'package:kubrs_app/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseAppCheck.instance
       .activate(androidProvider: AndroidProvider.debug);
