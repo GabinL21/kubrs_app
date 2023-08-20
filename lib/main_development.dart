821bfd6ab0670265efcd9f916b53a82fb06cbd91
gabin_laigle <gabin.laigle21@gmail.com>
2023-08-20 19:51:47 +0200

    feat: add new splash screen

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,15 +2,18 @@ import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:kubrs_app/app/app.dart';
 import 'package:kubrs_app/bootstrap.dart';
 import 'package:kubrs_app/firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseAppCheck.instance
       .activate(androidProvider: AndroidProvider.debug);
   await bootstrap(() => const App());
+  FlutterNativeSplash.remove();
 }
