81ec9ada1b08507c88cd4dd074ffb67247289751
gabin_laigle <gabin.laigle21@gmail.com>
2023-08-22 15:26:18 +0200

    feat(history): add style to solve details

--- lib/solve/view/solve_details_page.dart
+++ lib/solve/view/solve_details_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:kubrs_app/history/utils/date_time_formatter.dart';
+import 'package:kubrs_app/scramble/utils/scramble_visualizer.dart';
+import 'package:kubrs_app/scramble/view/scramble_visualization.dart';
 import 'package:kubrs_app/solve/model/solve.dart';
 
 class SolveDetailsPage extends StatelessWidget {
@@ -8,7 +11,9 @@ class SolveDetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SolveDetailsView(solve: solve));
+    return Scaffold(
+      body: SolveDetailsView(solve: solve),
+    );
   }
 }
 
@@ -19,14 +24,40 @@ class SolveDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _getSolveTime(solve),
-      ],
+    final theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(32, 96, 32, 96),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              _getScrambleVisualization(),
+              _getSolveTime(theme),
+              _getSolveDateTime(theme),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
-  Widget _getSolveTime(Solve solve) {
-    return Text(solve.timeToDisplay);
+  Widget _getScrambleVisualization() {
+    return ScrambleVisualizer.getUpFace(solve.scramble);
+  }
+
+  Widget _getSolveTime(ThemeData theme) {
+    return Text(
+      solve.timeToDisplay,
+      style: theme.textTheme.displayLarge,
+    );
+  }
+
+  Widget _getSolveDateTime(ThemeData theme) {
+    return Text(
+      DateTimeFormatter.format(solve.timestamp),
+      style: theme.textTheme.displayMedium
+          ?.copyWith(color: theme.colorScheme.secondary),
+    );
   }
 }
