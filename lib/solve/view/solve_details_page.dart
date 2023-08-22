622f1f4c4ee578762407762800204e07546477ab
gabin_laigle <gabin.laigle21@gmail.com>
2023-08-22 17:43:07 +0200

    feat(history): add app bar to solve details

--- lib/solve/view/solve_details_page.dart
+++ lib/solve/view/solve_details_page.dart
@@ -11,6 +11,7 @@ class SolveDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent),
       body: SolveDetailsView(solve: solve),
     );
   }
