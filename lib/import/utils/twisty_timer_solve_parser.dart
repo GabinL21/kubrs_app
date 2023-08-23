5dfe20d855f518f23589f32a2cb38ebf66cd56b7
gabin_laigle <gabin.laigle21@gmail.com>
2023-08-23 19:27:02 +0200

    feat(import): add twisty timer button

---
+++ lib/import/utils/twisty_timer_solve_parser.dart
@@ -0,0 +1,11 @@
+import 'package:kubrs_app/solve/model/solve.dart';
+
+class TwistyTimerSolveParser {
+  TwistyTimerSolveParser({required this.uid});
+
+  final String uid;
+
+  List<Solve> parseSolves(String textSolves) {
+    return List.empty();
+  }
+}
--- lib/import/view/import_dialog.dart
+++ lib/import/view/import_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kubrs_app/import/utils/cstimer_solve_parser.dart';
+import 'package:kubrs_app/import/utils/twisty_timer_solve_parser.dart';
 import 'package:kubrs_app/import/view/import_dialog_option.dart';
 
 class ImportDialog extends StatelessWidget {
@@ -16,6 +17,10 @@ class ImportDialog extends StatelessWidget {
         ImportDialogOption(
           timerName: 'csTimer',
           solveParser: CSTimerSolveParser(uid: uid).parseSolves,
+        ),
+        ImportDialogOption(
+          timerName: 'Twisty Timer',
+          solveParser: TwistyTimerSolveParser(uid: uid).parseSolves,
         ),
       ],
     );
