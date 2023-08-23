a81e36e9af7086df6a571ce7983b245d985d4225
gabin_laigle <gabin.laigle21@gmail.com>
2023-08-23 18:24:39 +0200

    refactor(import): extract import dialog options

--- lib/import/view/import_dialog.dart
+++ lib/import/view/import_dialog.dart
@@ -1,106 +1,20 @@
-import 'dart:io';
-
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:kubrs_app/import/utils/cstimer_solve_parser.dart';
-import 'package:kubrs_app/import/utils/solve_parser.dart';
-import 'package:kubrs_app/import/view/import_confirmation_dialog.dart';
-import 'package:kubrs_app/solve/model/solve.dart';
+import 'package:kubrs_app/import/view/import_dialog_option.dart';
 
 class ImportDialog extends StatelessWidget {
   const ImportDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
-      title: const Text('From which timer do you want to import solves?'),
+    return const SimpleDialog(
+      title: Text('From which timer do you want to import solves?'),
       children: [
-        _getOption('csTimer', CSTimerSolveParser.parseSolves, context),
+        ImportDialogOption(
+          timerName: 'csTimer',
+          solveParser: CSTimerSolveParser.parseSolves,
+        ),
       ],
-    );
-  }
-
-  Widget _getOption(
-    String timerName,
-    SolveParser solveParser,
-    BuildContext context,
-  ) {
-    final theme = Theme.of(context);
-    return SimpleDialogOption(
-      onPressed: () => _getOptionAction(solveParser, context),
-      child: Text(
-        timerName,
-        style: theme.textTheme.displaySmall
-            ?.copyWith(color: theme.colorScheme.secondary),
-      ),
-    );
-  }
-
-  void _getOptionAction(
-    SolveParser solveParser,
-    BuildContext context,
-  ) {
-    try {
-      _launchImportProcess(solveParser, context);
-    } catch (_) {
-      _handleError(context);
-    }
-  }
-
-  void _launchImportProcess(SolveParser solveParser, BuildContext context) {
-    FilePicker.platform
-        .pickFiles()
-        .then((result) => _processResult(result, solveParser, context))
-        .catchError((_) => _handleError(context));
-  }
-
-  void _processResult(
-    FilePickerResult? result,
-    SolveParser solveParser,
-    BuildContext context,
-  ) {
-    if (result == null || result.files.single.path == null) return;
-    final file = File(result.files.single.path!);
-    file
-        .readAsString()
-        .then(
-          (textSolves) => _processTextSolves(textSolves, solveParser, context),
-        )
-        .catchError((_) => _handleError(context));
-    ;
-  }
-
-  void _processTextSolves(
-    String textSolves,
-    SolveParser solveParser,
-    BuildContext context,
-  ) {
-    try {
-      final solves = solveParser.call(textSolves);
-      Navigator.pop(context); // Pop import dialog
-      _displayConfirmationDialog(solves, context);
-    } catch (_) {
-      _handleError(context);
-    }
-  }
-
-  void _displayConfirmationDialog(List<Solve> solves, BuildContext context) {
-    showDialog<ImportConfirmationDialog>(
-      context: context,
-      builder: (_) => ImportConfirmationDialog(solves: solves),
-    );
-  }
-
-  void _handleError(BuildContext context) {
-    Navigator.pop(context); // Pop import dialog
-    _displayErrorSnackBar(context);
-  }
-
-  void _displayErrorSnackBar(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Importation failed, check your file'),
-      ),
     );
   }
 }
---
+++ lib/import/view/import_dialog_option.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:kubrs_app/import/utils/solve_parser.dart';
+import 'package:kubrs_app/import/view/import_confirmation_dialog.dart';
+import 'package:kubrs_app/solve/model/solve.dart';
+
+class ImportDialogOption extends StatelessWidget {
+  const ImportDialogOption({
+    super.key,
+    required this.timerName,
+    required this.solveParser,
+  });
+
+  final String timerName;
+  final SolveParser solveParser;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SimpleDialogOption(
+      onPressed: () => _launchImportProcess(context),
+      child: Text(
+        timerName,
+        style: theme.textTheme.displaySmall
+            ?.copyWith(color: theme.colorScheme.secondary),
+      ),
+    );
+  }
+
+  void _launchImportProcess(BuildContext context) {
+    FilePicker.platform
+        .pickFiles()
+        .then((result) => _processResult(result, context));
+  }
+
+  void _processResult(FilePickerResult? result, BuildContext context) {
+    if (result == null || result.files.single.path == null) return;
+    Navigator.pop(context); // Pop import dialog
+    File(result.files.single.path!)
+        .readAsString()
+        .then((textSolves) => _processTextSolves(textSolves, context))
+        .catchError(
+          (_) => _displayErrorSnackBar(context), // Catch wrong file formats
+        );
+  }
+
+  void _processTextSolves(String textSolves, BuildContext context) {
+    try {
+      final solves = solveParser.call(textSolves);
+      _displayConfirmationDialog(solves, context);
+    } catch (_) {
+      _displayErrorSnackBar(context); // Catch parsing errors
+    }
+  }
+
+  void _displayConfirmationDialog(List<Solve> solves, BuildContext context) {
+    showDialog<ImportConfirmationDialog>(
+      context: context,
+      builder: (_) => ImportConfirmationDialog(solves: solves),
+    );
+  }
+
+  void _displayErrorSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Importation failed, check your file'),
+      ),
+    );
+  }
+}
